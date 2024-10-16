db73a31b44d2079496e62334a1c41aa8324581d0
Shirobokov-aa <136621836+Shirobokov-aa@users.noreply.github.com>
2024-10-17 01:06:22 +0300

    Update page.tsx

--- main/src/app/contact/page.tsx
+++ main/src/app/contact/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
                     height={24}
                   />
                 </span>
-                <span className="pl-2 w-full">
+                <span className="pl-2 w-full z-10">
                   54/6 Soi street 29 (birminghem), Somei district, birminghem
                   10220 united kingdom
                 </span>
